4e3f4b2245d8253975bb2c1d7a6cfba59d283709
eun <soc06241@naver.com>
2022-06-07 18:01:32 +0900

    16173

---
+++ DFS&BFS/src/BOJ_16173.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BOJ_16173 {
+	static int n;
+	static int [][] visited, arr;
+	
+	static Boolean DFS(int i, int j) {		
+		visited[i][j] = 1;
+		int val = arr[i][j];
+		if(val == -1)
+			return true;
+		
+		if(i+val < n && visited[i+val][j] == 0)
+			if(DFS(i+val, j))
+				return true;
+		if(j+val < n && visited[i][j+val] == 0)
+			if(DFS(i, j+val))
+				return true;
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		n = input.nextInt();
+		arr = new int[n][n];
+		visited = new int[n][n];
+		
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)
+				arr[i][j] = input.nextInt();
+		
+		System.out.println(DFS(0, 0) ? "HaruHaru" : "Hing");
+		
+		input.close();
+	}
+
+}
